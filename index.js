e0cae85893c4441b7ae47e53de529357388f58a1
Suman <86597620+SumanJ2000@users.noreply.github.com>
2022-06-10 12:44:39 +0530

    fourth

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ var date = new Date();
 var audio = new Audio("assets/alarm.mp3");
 audio.loop = true;
 
-//localList to fetch from  local storage at the beginnning
-// var localList = [];
-// localList.push(...JSON.parse(localStorage.getItem('alarms')));
+
 
 //Getting variables to use;
 var container = document.getElementById("container");
@@ -26,14 +24,7 @@ var clear_all = document.getElementById("clear_all");
 
 //Array to all Alarms Set By User
 var alarmList = [];
-// if (localList.length != 0) {
-//     alarmList.push(...localList);
-// }
 
-// if (alarmList.length != 0) {
-//     no_alarm.classList.remove('invisible');
-//     clear_all.classList.remove('invisible');
-// }
 displayNewAlarms();
 
 //To have Clock current and actual time and update time at every Second;
